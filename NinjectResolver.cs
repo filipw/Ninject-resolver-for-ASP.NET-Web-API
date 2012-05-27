89a8bd996e26d3df508f4fa8e5fd81f253b8a564
Filip W <wojcieszyn@gmail.com>
2012-05-27 11:11:51 +0200

    Added NinjectResolver and NinjectScope classes

---
+++ NinjectResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject.Syntax;
+using System.Web.Http.Dependencies;
+using Ninject.Activation;
+using Ninject.Parameters;
+using Ninject;
+
+namespace aspnetwebapi_ninject.Infrastructure
+{
+    public class NinjectResolver : NinjectScope, IDependencyResolver
+    {
+        private IKernel _kernel;
+
+        public NinjectResolver(IKernel kernel)
+            : base(kernel)
+        {
+            _kernel = kernel;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new NinjectScope(_kernel.BeginBlock());
+        }
+    }
+}
